365b1a640e7b1e41a686579ce09ead2f1842c784
SpaceDassie <cjdjcj@gmail.com>
2019-02-21 23:00:28 +0200

    working on fps

---
+++ Pantoffel/Assets/Scripts/Player/BasicFPSController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicFPSController : MonoBehaviour {
+    public float playerMoveSpeed = 5.0f;
+
+    private float playerHorizontal;
+    private float playerVertical;
+
+    void Start ()
+
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update () {
+        playerVertical = Input.GetAxis ("Vertical") * playerMoveSpeed * Time.deltaTime;
+        playerHorizontal = Input.GetAxis ("Horizontal") * playerMoveSpeed * -Time.deltaTime;
+        transform.Translate (playerVertical, 0, playerHorizontal);
+
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
