646f7fd815a7c65d069ff0ea8b159353b3ab0b69
SpaceDassie <cjdjcj@gmail.com>
2019-02-22 02:54:25 +0200

    still working on controller

--- Pantoffel/Assets/Scripts/Player/PlayerLook.cs
+++ Pantoffel/Assets/Scripts/Player/PlayerLook.cs
@@ -2,37 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLook : MonoBehaviour
-{
+public class PlayerLook : MonoBehaviour {
     [SerializeField] private string mouseXInputName, mouseYInputName;
-    [SerializeField] private float mouseSensitivity; 
+    [SerializeField] private float mouseSensitivity;
 
+    [SerializeField] private Transform playerBody;
 
-    private void Awake() 
-    {
-        LockCursor();
+    private float xAxisClamp;
+
+    private void Awake () {
+        LockCursor ();
+        xAxisClamp = 0.0f;
     }
 
-    private void LockCursor()
-    {
+    private void LockCursor () {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void Update() 
-    {
-        CameraRotation();
-    
+    private void Update () {
+        CameraRotation ();
     }
 
+    private void CameraRotation () {
+        float mouseX = Input.GetAxis (mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis (mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
-    void CameraRotation()
-    {
-        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+        xAxisClamp += mouseY;
 
-        transform.Rotate(-transform.right * mouseY);
+        if (xAxisClamp > 90.0f) {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue (270.0f);
+        } else if (xAxisClamp < -90.0f) {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue (90.0f);
+        }
+
+        transform.Rotate (Vector3.left * mouseY);
+        playerBody.Rotate (Vector3.up * mouseX);
     }
 
-
-
+    private void ClampXAxisRotationToValue (float value) {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
 }
