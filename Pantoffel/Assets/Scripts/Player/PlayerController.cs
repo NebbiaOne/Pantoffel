d7df6b930310a3989bf72ea5faa43321c20d5b38
SpaceDassie <cjdjcj@gmail.com>
2019-02-24 01:33:35 +0200

    PlayerController + test environment
    Working on new PlayerController and working in test environment

---
+++ Pantoffel/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+    private float speed = 5f;
+
+
+    private PlayerMotor motor;
+
+     void Start() 
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+
+    void Update() 
+    {
+        //calculate movement velocity as 3d vector
+
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");   
+
+        Vector3 _movHorizontal = transform.right * _xMov; // (1,0,0)
+        Vector3 _movVertical = transform.forward * _zMov; // (0,0,1)
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed; // Alway get vector with length of 1 -- Final movement vector
+
+        motor.Move(_velocity); //Apply movement
+
+
+    }
+
+
+}
---
+++ Pantoffel/Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    private Rigidbody rb;
+
+    
+
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void Move (Vector3 _velocity)  //Gets a movement vector
+    {
+        velocity = _velocity;
+    }
+
+    void FixedUpdate() //run every physics iteration
+    {
+        PerformMovement();
+    }
+
+
+    void PerformMovement()
+    {
+        
+    }
+
+   
+
+
+}
