fee4043f4bd0b7c4a9e2649e6563a31924e80bfe
SpaceDassie <cjdjcj@gmail.com>
2019-02-24 21:46:12 +0200

    grab mechanoc
    working on grabbing objects

---
+++ Pantoffel/Assets/Scripts/Objects/InteractableObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+
+
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnMouseDown() 
+    {
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = tempParent.transform;        
+        
+    }
+
+    void OnMouseUp() 
+    {
+        item.GetComponent<Rigidbody>().useGravity =true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.transform.parent = null;        //Might need to change this later if object should have other parent eg. currentParent and newParent
+        item.transform.position = guide.transform.position;
+    }
+}
