98975daa443a10f9f380d1e82a0bfbeb4d412d1a
SpaceDassie <cjdjcj@gmail.com>
2019-02-28 21:35:59 +0200

    interact
    working on player object interaction

--- Pantoffel/Assets/Scripts/Objects/InteractableObject.cs
+++ Pantoffel/Assets/Scripts/Objects/InteractableObject.cs
@@ -2,42 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObject : MonoBehaviour
-{
-
+public class InteractableObject : MonoBehaviour {
 
     public GameObject item;
     public GameObject tempParent;
     public Transform guide;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        item.GetComponent<Rigidbody>().useGravity = true;
+    void Start () {
+        item.GetComponent<Rigidbody> ().useGravity = true;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 
-
-    void OnMouseDown() 
-    {
-        item.GetComponent<Rigidbody>().useGravity = false;
-        item.GetComponent<Rigidbody>().isKinematic = true;
+    void OnMouseDown () {
+        item.GetComponent<Rigidbody> ().useGravity = false;
+        item.GetComponent<Rigidbody> ().isKinematic = true;
+        item.GetComponent<Collider> ().enabled = false;
         item.transform.position = guide.transform.position;
         item.transform.rotation = guide.transform.rotation;
-        item.transform.parent = tempParent.transform;        
-        
+        item.transform.parent = tempParent.transform;
+
     }
 
-    void OnMouseUp() 
-    {
-        item.GetComponent<Rigidbody>().useGravity =true;
-        item.GetComponent<Rigidbody>().isKinematic = false;
-        item.transform.parent = null;        //Might need to change this later if object should have other parent eg. currentParent and newParent
+    void OnMouseUp () {
+        item.GetComponent<Rigidbody> ().useGravity = true;
+        item.GetComponent<Rigidbody> ().isKinematic = false;
+        item.transform.parent = null; //Might need to change this later if object should have other parent eg. currentParent and newParent
         item.transform.position = guide.transform.position;
+        item.GetComponent<Collider> ().enabled = true;
     }
 }
