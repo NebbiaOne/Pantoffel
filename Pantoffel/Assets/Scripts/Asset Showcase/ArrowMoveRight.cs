7acf33e7db53e73f5fbd83348c6aeea3de6fb475
SpaceDassie <cjdjcj@gmail.com>
2019-04-08 23:40:04 +0200

    ASSET SHOWCASE
    Added an asset showcase scene, with some scripts
    
    -- Left and right arrows to move asset parent 5 units L/R

---
+++ Pantoffel/Assets/Scripts/Asset Showcase/ArrowMoveRight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMoveRight : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.RightArrow)) {
+            this.transform.Translate (5, 0, 0);
+            //transform.Translate (Vector3.left * -1 * Time.deltaTime);
+        }
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+            this.transform.Translate (-5, 0, 0);
+            //transform.Translate (Vector3.left * 1 * Time.deltaTime);
+        }
+    }
+}
